8f80d374a9efa5b7ad0f7f1034dade5e09e99e40
Denis luz <75055532+alhoCoro@users.noreply.github.com>
2025-03-18 11:15:57 -0300

    média.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ b1 = float(input("Nota da primeira prova: "))
 b2 = float(input("Nota da segunda prova: "))
 b3 = float(input("nota da terceira prova: "))
 b4 = float(input("nota da quarta prova: "))
-media = b1+b2+b3+b4/4
+media = (b1+b2+b3+b4)/4
 print(f"sua média foi de: {media}")
 if media < 20:
     {
